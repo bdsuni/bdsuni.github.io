61a3cc54012506174e67e0daa8a2f363d25f7ec4
Kiran Pankan <kiran@plank.life>
2020-11-10 16:29:27 +0530

    map mail

--- loop/assets/ajax/mail.php
+++ loop/assets/ajax/mail.php
@@ -1,12 +1,12 @@
 <?php
-$to = "support@loopmobility.co";
+$to = "pnknbkup@gmail.com";
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
 
 	$message = "First name: $first_name\nLast name: $last_name\nEmail: $email\nMessage: $message";
-	$subject = "=?utf-8?B?".base64_encode("message from the new site")."?=";
 	$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/plain; charset=utf-8\r\n";
 
 	if(mail($to, $subject, $message, $headers))
